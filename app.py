19df55ab30bae69386fbfb28ff352cb3177e917f
Sergey <thankfulcon@gmail.com>
2023-04-25 02:32:54 -0400

    final touch, finished model loading

--- app.py
+++ app.py
@@ -3,23 +3,21 @@ from src.evaluate import evaluate_prompt, model_list
 
 st.title("Toxic Tweets")
 
+# description of the project
 st.info("This NLP machine learning project aims to predict the toxicity level of input tweets using fine-tuning techniques on a pre-trained language model. The project utilizes Docker containers for efficient deployment and management, while Hugging Face Spaces and Transformers provide the necessary libraries and tools for building and training the model. The model is trained on a large dataset of labeled toxic tweets to enable it to classify new input tweets as toxic or non-toxic. This project can help improve online safety by automatically flagging potentially harmful content.")
 
 # variables defined
 sentiment_model_names = model_list()
 section1, section2 = st.columns(2)
 
-# functions
+# function to predict the output
 def predict(model_name, prompt):
-    output = evaluate_prompt(model_name, prompt)[0]
-    label = output["label"]
-    score = output["score"]
+    output = evaluate_prompt(model_name, prompt)
     with section2:
-        st.write("Label:", label)
-        st.write("Score:", score)
+        st.table(output)
         st.success("Completed!")
 
-
+# main code
 with section1:
     st.header("Input")
     prompt = st.text_area("Prompt", "Eat Up Every Moment")
--- src/evaluate.py
+++ src/evaluate.py
@@ -1,14 +1,47 @@
 from transformers import pipeline, AutoModelForSequenceClassification, AutoTokenizer
+from transformers import DistilBertTokenizerFast, DistilBertForSequenceClassification
+import torch
+import numpy as np
+import pandas as pd
 
+# Public function called from app.py to evaluate the prompt
 def evaluate_prompt(model_name, prompt):
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForSequenceClassification.from_pretrained(model_name)
-    
-    classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
-    return classifier(prompt)
+    # Check if the model is fine-tuned-toxic-tweets
+    if model_name == "fine-tuned-toxic-tweets":
+        return eval_fine_tuned_toxic_tweets(model_name, prompt)
+    else: # If not, use the pipeline function
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        model = AutoModelForSequenceClassification.from_pretrained(model_name)
+        classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+        return classifier(prompt)
 
+# Private function to evaluate the prompt using the fine-tuned-toxic-tweets model
+def eval_fine_tuned_toxic_tweets(model_name, prompt):
+    # Load the model and tokenizer
+    model = DistilBertForSequenceClassification.from_pretrained("sergey-hovhannisyan/fine-tuned-toxic-tweets")
+    tokenizer = DistilBertTokenizerFast.from_pretrained("distilbert-base-uncased")
+    encoded_text = tokenizer(prompt, truncation=True, padding=True, return_tensors='pt')
+
+    with torch.no_grad():
+        output = model(**encoded_text)
+
+    # Get the labels and scores
+    labels = np.array(["toxic", "severe", "obscene", "threat", "insult", "identity hate"])
+    scores = torch.sigmoid(output.logits)*100
+    scores = scores.numpy()
+
+    # Sort the scores in descending order
+    sort_idx = np.flip(np.argsort(scores))
+    labels = labels[sort_idx]
+    scores = scores[0][sort_idx]
+    
+    # Return the labels and scores as a dataframe
+    return pd.DataFrame({"Label": labels[0].tolist(), "Score": scores[0].tolist()}).set_index("Label")
+
+# List of models to choose from
 def model_list():
     sentiment_models = [
+        "fine-tuned-toxic-tweets",
         "distilbert-base-uncased-finetuned-sst-2-english",
         "bert-base-uncased",
         "albert-base-v2"]
