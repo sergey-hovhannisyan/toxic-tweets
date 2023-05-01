cf90a8825894d33de8add0a40202400349084cd2
Sergey Hovhannisyan <thankfulcon@gmail.com>
2023-05-01 04:07:14 -0400

    Update app.py info section

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ from src.evaluate import evaluate_prompt, model_list
 st.title("Toxic Tweets")
 
 # description of the project
-st.info("This NLP machine learning project aims to predict the toxicity level of input tweets using fine-tuning techniques on a pre-trained language model. The project utilizes Docker containers for efficient deployment and management, while Hugging Face Spaces and Transformers provide the necessary libraries and tools for building and training the model. The model is trained on a large dataset of labeled toxic tweets to enable it to classify new input tweets as toxic or non-toxic. This project can help improve online safety by automatically flagging potentially harmful content.")
-
+st.info("This app utilizes a multi-head classifier that is fine-tuned to evaluate the toxicity level of a given prompt, providing 6 labels and their corresponding toxicity scores in descending order. By utilizing pre-trained language models, large labeled datasets, and fine-tuning techniques, the app helps determine if the prompt is toxic or not and contributes to enhancing online safety.")
 # variables defined
 sentiment_model_names = model_list()
 section1, section2 = st.columns(2)
